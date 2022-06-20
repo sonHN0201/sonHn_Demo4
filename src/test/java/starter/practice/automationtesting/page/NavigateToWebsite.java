c3c5b2979c67c4c6e76c74fa0f8544c1c7d9cab1
sonHN0201 <sonhn85021@co-well.vn>
2022-06-20 16:20:31 +0700

    commit  code 20/06/2022

--- src/test/java/starter/practice/automationtesting/page/NavigateToWebsite.java
+++ src/test/java/starter/practice/automationtesting/page/NavigateToWebsite.java
@@ -2,7 +2,9 @@ package starter.practice.automationtesting.page;
 
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.WebDriver;
 
 @DefaultUrl("page:base.url")
 public class NavigateToWebsite extends PageObject {
+
 }
